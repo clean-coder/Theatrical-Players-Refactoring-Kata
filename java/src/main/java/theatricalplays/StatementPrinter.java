6bb00537acd44e44098be46b91c28c602108e1b7
clean-coder <jean-claude.brantschen@gmx.ch>
2025-01-10 12:40:42 +0100

    extract formatting into a new method formatInvoice()

--- java/src/main/java/theatricalplays/StatementPrinter.java
+++ java/src/main/java/theatricalplays/StatementPrinter.java
@@ -13,6 +13,10 @@ public class StatementPrinter {
     public String print(Invoice invoice, Map<String, Play> plays) {
         var second = calculateInvoice(invoice, plays);
 
+        return formatInvoice(invoice, second);
+    }
+
+    private static String formatInvoice(Invoice invoice, ArrayList<Container> second) {
         var result = String.format("Statement for %s\n", invoice.customer);
         NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
         for (var item : second) {
